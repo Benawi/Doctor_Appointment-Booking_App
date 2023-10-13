2ef6895520202a77ab1d0912e2e33f6b4ff027f8
toyman640 <falakoadebayor@gmail.com>
2023-10-13 12:21:22 +0100

    Set up project

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello World from react!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
---
+++ app/javascript/controllers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const  App = () => {
+  return (
+    <div>
+      <p>This is from react</p>
+    </div>
+  );
+}
+
+export default App;
