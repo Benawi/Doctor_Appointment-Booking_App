92e6ba315356df2d2c4cc325ce80d28af69b255c
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-14 19:23:03 +0300

    Create doctors migrations

--- db/migrate/20231014160333_create_doctors.rb
+++ db/migrate/20231014160333_create_doctors.rb
@@ -1,8 +1,13 @@
 class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors do |t|
+      t.string :docname
+      t.string :location
+      t.string :fullname
+      t.references :specialization_area, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :doctors, :docname, unique: true
   end
 end
