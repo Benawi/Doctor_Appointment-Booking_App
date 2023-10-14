327fc30b661e644b47154e09677228b1bab25ca0
toyman640 <falakoadebayor@gmail.com>
2023-10-14 12:50:36 +0100

    Update app file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World from react!</h1>);
+  return (<h1>Hello World from react! hello. This is second session</h1>);
 }
 
 ReactDOM.render(
