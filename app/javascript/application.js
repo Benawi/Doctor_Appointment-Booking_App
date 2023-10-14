0e0acb10dfa8f5739d180dedc59407206cda5a69
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-14 13:47:34 +0300

    config setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World from react!</h1>);
+  return (
+    <h1> Hello Toyo..... are you doing from reactrr chech with test test it again!</h1>
+);
 }
 
 ReactDOM.render(
