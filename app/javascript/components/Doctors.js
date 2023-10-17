3318394cfd6f80be09e7b8eb1a6eb44ad1c2d1c4
toyman640 <falakoadebayor@gmail.com>
2023-10-17 11:48:28 +0100

    Update doctors

--- app/javascript/components/Doctors.js
+++ app/javascript/components/Doctors.js
@@ -3,8 +3,17 @@ import React from "react";
 const Doctors = () => {
   return (
     <div className="Main-Section">
+      Add Index  Add Column 
+      <div>
+        <h2>Doctors component</h2>
+        <div>
+          <p>Doctor's image</p>
+          <p>Doctor's Nmae</p>
+          <p>Doctor's Bio</p>
+          <p></p>
+        </div>
 
-      <h2>Doctors component</h2>
+      </div>
     </div>
   );
 }
