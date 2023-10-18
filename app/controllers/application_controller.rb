011d3075856069fdd9e9e60e5a21a9785623530f
toyman640 <falakoadebayor@gmail.com>
2023-10-18 14:31:10 +0100

    Update sign for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation])
   end
 end
