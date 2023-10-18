b2a1e2fddf76e1b7fc9ea1bebfe4fa763e64fb0c
toyman640 <falakoadebayor@gmail.com>
2023-10-18 15:04:22 +0100

    Update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, authentication_keys: [:username]
 
+  #relations
+  has_many :reservations, foreign_key: 'user', dependent: :destroy
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
