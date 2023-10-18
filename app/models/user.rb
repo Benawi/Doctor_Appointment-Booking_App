43567f0aa587df52852904597d45e8e166aa6756
toyman640 <falakoadebayor@gmail.com>
2023-10-18 13:33:19 +0100

    Set up user login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
