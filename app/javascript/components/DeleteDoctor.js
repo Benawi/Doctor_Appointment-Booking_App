91d13887257c674089fc2dc83f54b594e5d87b58
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-20 21:48:30 +0300

    update styles

--- app/javascript/components/DeleteDoctor.js
+++ app/javascript/components/DeleteDoctor.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  deleteDoctorAction,
+  getDoctorsAction,
+} from '../redux/doctorsReducer/doctors';
+import '../../assets/stylesheets/delete_doctor.css';
+
 
 const DeleteDoctor = () => {
-  return (
-    <div className="Main-Section">
+  const dispatch = useDispatch();
+  const doctors = useSelector(state => state.doctors.doctors);
+  const [successNotice, setSuccessNotice] = useState(false);
 
-      <h2>Delete docotor component</h2>
+  const deleteDoctor = (id) => {
+    dispatch(deleteDoctorAction(id));
+    setSuccessNotice(true);
+    setTimeout(() => {
+      setSuccessNotice(false);
+    }, 2000);
+  };
+  
+  return (
+    <div className="delete-container">
+      <table>
+      <thead>
+        <tr>
+          <th>Photo</th>
+          <th>Name</th> 
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {doctors.map(doctor => (
+          <tr key={doctor.id} className="delete-details">
+             <td> <img src={`${doctor.photo}`} alt="doctor" className="delete-image" /></td>
+             <td>{doctor.name}</td>
+           <td>  <button
+            type="button"
+            onClick={() => deleteDoctor(doctor.id)}
+            className="delete-button"
+          >
+            Delete
+          </button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+      {successNotice && (
+        <p className="text-center text-sky-500 text-lg mt-4">
+          Doctor deleted succesfully!
+        </p>
+      )}
     </div>
   );
-}
+};
 
 export default DeleteDoctor;
