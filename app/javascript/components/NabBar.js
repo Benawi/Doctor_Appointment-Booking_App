550ced5aab9b120f912c86d135bb577274ee2dcb
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-20 12:44:56 +0300

    Add styles to naviagtions

--- app/javascript/components/NabBar.js
+++ app/javascript/components/NabBar.js
@@ -1,44 +1,89 @@
-import React  from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import '../../assets/stylesheets/main.css';
+import logo from '../../assets/images/doctor-logo.png';
+import fb from '../../assets/images/fb-icon-home.png';
+import twitter from '../../assets/images/twitter-icon-home.png';
+import insta from'../../assets/images/insta-icon-home.png';
+
+const social = [
+  { icon: twitter },
+  { icon: fb },
+  { icon: insta },
+  
+];
 
 const NavBar = () => {
-  return(
-    <div className="Navigation">
-      <p>Logo</p>
-      <ul className="Menu-Items">
-        <li>
-          <NavLink to='/'>DOCTORS</NavLink>
-        </li>
-        <li>
-          <NavLink to='/reserve-form'>RESERVE FORM</NavLink>
-          
-        </li>
-        <li>
-          <NavLink to='/my-reservation'>MY RESERVATION</NavLink>
-          
+
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <Link
+          to="/"
+        >
+          <img src={logo} className="logo" alt="logo" />
+        </Link>
+      </div>
+      <div className="links-container">
+        <ul>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? 'active link' : 'link')}
+              to="/doctors"
+            >
+              Doctors
+            </NavLink>
           </li>
-        <li>
-          <NavLink to='/add-doctor'>ADD DOCTOR</NavLink>
-        </li>
-        <li>
-          <NavLink to='/delete-doctor'>DELETE DOCTOR</NavLink>
-        </li>
-        <li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? 'active link' : 'link')}
+              to="/reserve-form"
+            >
+              reserve form
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? 'active link' : 'link')}
+              to="/my-reservation"
+            >
+            My Reservations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? 'active link' : 'link')}
+              to="/add-doctor"
+            >
+              add doctor
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? 'active link' : 'link')}
+              to="/delete-doctor"
+            >
+              Delete a Doctor
+            </NavLink>
+          </li>
+          <li>
           <NavLink to='/users/sign_out'>Sign Out</NavLink>
-        </li>
-      </ul>
-      <div>
-        <ul className="Footer-contents">
-          <li>Facebook</li>
-          <li>Facebook</li>
-          <li>Facebook</li>
-          <li>Facebook</li>
-          <li>Facebook</li>
-          <p>Copyright</p>
+          </li>
         </ul>
       </div>
-    </div>
+      <div className="social-net socialicon">
+        {social.map((item) => (
+          <a href="/" key={item.icon}>
+            <img src={item.icon} alt="social" className="w-8" />
+          </a>
+        ))}
+      </div>
+      <p className="copyright">&copy; 2023 Bena & Toyo</p>
+    </nav>
+    
   );
-}
+  
+};
+
 
 export default NavBar;
