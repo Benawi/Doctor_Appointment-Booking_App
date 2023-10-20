b754614156c85db660b325b53500dd28fc06df3c
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-20 12:17:43 +0300

    Add components to delete list of doctors

---
+++ app/javascript/components/DeleteDoctor.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DeleteDoctor = () => {
+  return (
+    <div className="Main-Section">
+
+      <h2>Delete docotor component</h2>
+    </div>
+  );
+}
+
+export default DeleteDoctor;
