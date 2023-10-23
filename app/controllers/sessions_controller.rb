1f971a9543194155959454ff6bb7d5f5b3159a85
toyman640 <falakoadebayor@gmail.com>
2023-10-23 15:58:47 +0100

    Add method to end user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < Devise::SessionsController
+  protect_from_forgery with: :null_session
   def destroy
     sign_out(current_user) # For Devise, sign the user out
     # If using token-based authentication, invalidate the token here
