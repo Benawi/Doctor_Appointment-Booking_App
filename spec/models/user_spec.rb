ae4b66ace33e7ddb8d7c29e3e01941eee13f94ee
toyman640 <falakoadebayor@gmail.com>
2023-10-24 11:18:15 +0100

    Add test to create user when all requirements are provided

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'should not save user without name' do
+  it 'should not save user without username' do
     user = User.new
     expect(user.valid?).to eq false
   end
+
+  it 'should save user with valid attributes' do
+    user = User.new(username: 'toyman', email: 'falako@mail.com', password: 'toyman123', password_confirmation: 'toyman123')
+    expect(user.valid?).to eq true
+  end
+
 
 end
