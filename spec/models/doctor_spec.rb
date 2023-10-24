47b0aaf03b7a016685f5b202d285600c33e28f03
toyman640 <falakoadebayor@gmail.com>
2023-10-24 12:44:34 +0100

    Fix linters errors

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -12,7 +12,8 @@ RSpec.describe Doctor, type: :model do
   end
 
   it 'should save doctor with valid attributes' do
-    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top notch doctor', photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization1.id)
+    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top notch doctor',
+                        photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization1.id)
     expect(doctor.valid?).to eq true
   end
 
@@ -22,12 +23,14 @@ RSpec.describe Doctor, type: :model do
   end
 
   it 'should not save doctor if specialization is nill' do
-    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top notch doctor', photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization2.id)
+    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top notch doctor',
+                        photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization2.id)
     expect(doctor.specialization_id).not_to be_nil
   end
 
   it 'should return the correct specialization name' do
-    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top-notch doctor', photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization: @specialization1)
+    doctor = Doctor.new(name: 'Ben Cruise', bio: 'A top-notch doctor',
+                        photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization: @specialization1)
     expect(doctor.specialization_name).to eq 'Dermatologist'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe User, type: :model do
   end
 
   it 'should save user with valid attributes' do
-    user = User.new(username: 'toyman', email: 'falako@mail.com', password: 'toyman123', password_confirmation: 'toyman123')
+    user = User.new(username: 'toyman', email: 'falako@mail.com', password: 'toyman123',
+                    password_confirmation: 'toyman123')
     expect(user.valid?).to eq true
   end
 
   it 'should not save if user passwords do not match' do
-    user = User.new(username: 'jane007', email: 'jane@mail.com', password: 'toyman12', password_confirmation: 'toyman123')
+    user = User.new(username: 'jane007', email: 'jane@mail.com', password: 'toyman12',
+                    password_confirmation: 'toyman123')
     expect(user.valid?).to eq false
   end
 
@@ -20,5 +22,4 @@ RSpec.describe User, type: :model do
     user = User.new(username: 'jane007', password: 'toyman12', password_confirmation: 'toyman123')
     expect(user.valid?).to eq false
   end
-
 end
