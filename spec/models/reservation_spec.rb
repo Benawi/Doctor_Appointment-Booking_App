f3ac70fa4513e929584e161ad20cc9eb77dd3738
toyman640 <falakoadebayor@gmail.com>
2023-10-24 12:25:56 +0100

    Add test not save user if reservation has no attributes

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Doctor, type: :model do
+  before(:each) do
+
+    @user1 = User.create(username: 'toyman', email: 'falako@mail.com', password: 'toyman123', password_confirmation: 'toyman123')
+    @user2 = User.create(username: 'Lizzy', email: 'elizabeth@mail.com', password: 'lily123', password_confirmation: 'lily123')
+
+    @specialization1 = Specialization.create(name: 'Dermatologist')
+    @specialization2 = Specialization.create(name: 'Pediatrician')
+    @doctor1 = Doctor.create(name: 'Ben Cruise', bio: 'A top notch doctor', photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization1.id)
+    @doctor2 = Doctor.create(name: 'Jene Cartwright', bio: 'A top notch doctor', photo: 'https://unsplash.com/photos/man-in-white-suit-jacket-7bMdiIqz_J4.jpg', specialization_id: @specialization2.id)
+  end
+
+  it 'should not save a reservation without parameters' do
+    reservation = Reservation.new
+    expect(reservation.valid?).to eq false
+  end
+
+ 
+end
