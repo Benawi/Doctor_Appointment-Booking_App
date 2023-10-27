9b21b48a3c81188aa821add2463b55d281ba9aee
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-27 10:09:51 +0300

    Update config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,5 @@ cp -a app/assets/builds/. public/
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:create
-bundle exec rails db:migrate
+bundle exec rake db:migrate
+bundle exec rails db:seed
